bcedbc63e5e1fa32455d6416dd13686eb169c1b4
YoungJoong-kim98 <rladudwnd012@gmail.com>
2025-01-27 12:56:07 +0900

    1주차1번과제

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("이름을 입력해주세요.");
+            string Name = Console.ReadLine();
+            Console.WriteLine("나이를 입력해주세요.");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine($"나의 이름은 {Name}이고 나이는{Age}입니다.");
         }
     }
 }
