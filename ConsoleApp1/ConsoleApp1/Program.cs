dc837c14bb32c64c0ea5f19efbb3dd9c762edc07
YoungJoong-kim98 <rladudwnd012@gmail.com>
2025-01-27 20:03:57 +0900

    2주차1번과제 숫자맞추기 게임
    컴퓨터 랜덤 값 중복방지로 응용

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,71 @@
-﻿namespace ConsoleApp1
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕하세요 BMI 수치를 알려드리겠습니다.");
-            Console.Write("체중을 Kg 단위로 입력하세요.");
-            float Weight = float.Parse(Console.ReadLine());
-            Console.Write("키를 Cm 단위로 입력하세요.");
-            float Height = float.Parse(Console.ReadLine())/100;
-            float BMI = Weight / (Height * Height); // BMI 계산 공식 체중 / 신장제곱
-            Console.WriteLine(BMI);
+            int[] com = new int[3] {0,0,0}; // 컴퓨터 랜덤 값을 저장 할 배열
+            int[] user = new int[3]; // 사용자가 입력할 값을 저장 할 배열
+            int count = 0; // 횟수를 저장 할 변수
+            int z = 0; // while 문에 쓰일 변수
+            while (true) // 중복 숫자가 들어가지 못하도록 방지
+            {
+                
+                com[z] = new Random().Next(1,10);
+                z++;
+                if(z>=3)
+                {
+                    if (com[0] == com[1] || com[0] == com[2] || com[1] == com[2])
+                    {
+                        z = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(com[i]); // 컴퓨터 랜덤 값 확인
+            }
 
+
+
+            while(true)
+            {
+                count++; // 횟수 
+                int correct = 0; //맞춘 숫자 초기화
+                Console.WriteLine("컴퓨터의 1~9까지 3개의 숫자를 맞춰보세요");
+                for (int i = 0; i<3;i++)
+                {
+                    user[i] = int.Parse(Console.ReadLine());
+                }
+
+                for (int i = 0;i<3;i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (com[i] == user[j])
+                        {
+                            correct++;
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine("시도" + count + " : " + correct + "개의 숫자를 맞추셨습니다.");
+
+                if(correct == 3)
+                {
+                    Console.WriteLine("축하합니다.! 모든 숫자를 맞추셨습니다.!");
+                    break;
+                }
+            }
+            
         }
     }
 }
