f361478aa0a93c8db3c338530dc0517acbc05505
YoungJoong-kim98 <rladudwnd012@gmail.com>
2025-01-27 12:56:32 +0900

    1주차2번과제

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,12 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("이름을 입력해주세요.");
-            string Name = Console.ReadLine();
-            Console.WriteLine("나이를 입력해주세요.");
-            string Age = Console.ReadLine();
-
-            Console.WriteLine($"나의 이름은 {Name}이고 나이는{Age}입니다.");
+            Console.Write("첫 번째 숫자를 입력하세요.");
+            int Num1 = int.Parse(Console.ReadLine());
+            Console.Write("두 번째 숫자를 입력하세요.");
+            int Num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두 수의 사칙연산 결과입니다.");
+            Console.WriteLine("더하기 " + (Num1 + Num2));
+            Console.WriteLine("빼기 " + (Num1 - Num2));
+            Console.WriteLine("곱하기 " + Num1 * Num2);
+            Console.WriteLine("나누기 " + Num1 / Num2);
+            Console.WriteLine("나머지 " + Num1 % Num2);
         }
     }
 }
